cf917e43a0b8b51d75abb6dcd25cab6e4ace27b1
vadecha shreyash <vadechashreyash07@gmail.com>
2025-12-28 20:25:12 +0530

    add render URL

--- src/pages/Linear.jsx
+++ src/pages/Linear.jsx
@@ -26,7 +26,7 @@ function Linear() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://ml-models-backend-i72f.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
