bc5e857e07b206ca729edcd36ab6783b427d2534
vadecha shreyash <vadechashreyash07@gmail.com>
2026-01-01 16:56:36 +0530

    update backend url with render

--- src/pages/Linear.jsx
+++ src/pages/Linear.jsx
@@ -26,7 +26,7 @@ function Linear() {
     setLoading(true);
 
     try {
-      const response = await fetch("https://ml-models-backend-i72f.onrender.com/predict-linear", {
+      const response = await fetch("https://ml-models-backend-80sn.onrender.com/predict-linear", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
